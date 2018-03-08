38fd93147fa224ee6b5eace1cd1e4d3caf7e5385
Ian Yocum <ian.yocum@pearson.com>
2018-03-08 14:33:37 -0600

    Manage Profile Issues [AC-935]
    Part of Microsoft Accessibility Update [AC-891]
    
    Re-did the fixes from `feature/891_939_manage_account_issues` but
    based on the 2.5.1 branch instead. More recent updates on the
    `master` branch break our site's use of the library.

--- croppie.js
+++ croppie.js
@@ -659,7 +659,10 @@
 
         self._currentZoom = ui ? ui.value : self._currentZoom;
         transform.scale = self._currentZoom;
-        self.elements.zoomer.setAttribute('aria-valuenow', self._currentZoom);
+        var adjusted_value = parseFloat(self._currentZoom) * 100;
+        adjusted_value = Math.round(adjusted_value * 10) / 10;
+        self.elements.zoomer.setAttribute('aria-valuenow', adjusted_value + ' percent');
+        self.elements.zoomer.setAttribute('aria-label', 'zoom ' + adjusted_value + ' percent');
         applyCss();
 
         if (self.options.enforceBoundary) {
